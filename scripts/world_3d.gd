c3df6d6e3d22c1542ba4a91be67e8286e89b5eaf
yovahn <yyrhoole@gmail.com>
2026-08-29 23:04:37 -0700

    Add shrine return objective

--- scripts/world_3d.gd
+++ scripts/world_3d.gd
@@ -2,14 +2,26 @@ extends Node3D
 
 @onready var player: CharacterBody3D = $Player
 @onready var crystal: MeshInstance3D = $CrystalDestination
+@onready var shrine: MeshInstance3D = $ReturnShrine
 @onready var prompt: Label = $HUD/Prompt
 @onready var objective: Label = $HUD/Objective
 
 var crystal_collected := false
+var adventure_complete := false
 var interact_released := true
 
 func _process(_delta: float) -> void:
+	if adventure_complete:
+		return
 	if crystal_collected:
+		var at_shrine := player.global_position.distance_to(shrine.global_position) < 2.5
+		prompt.text = "Press E to return the crystal" if at_shrine else "Return to the blue shrine"
+		if Input.is_key_pressed(KEY_E) and interact_released and at_shrine:
+			adventure_complete = true
+			prompt.text = "Adventure complete!"
+			objective.text = "You restored the shrine"
+			interact_released = false
+		if not Input.is_key_pressed(KEY_E): interact_released = true
 		return
 	var nearby := player.global_position.distance_to(crystal.global_position) < 2.2
 	prompt.text = "Press E to collect the crystal" if nearby else ""
