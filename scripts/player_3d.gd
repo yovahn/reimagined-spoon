a1210709c4dd105fc2c2e76a0d78594127bedab4
yovahn <yyrhoole@gmail.com>
2026-08-29 22:57:16 -0700

    Make 3D world walkable

---
+++ scripts/player_3d.gd
@@ -0,0 +1,17 @@
+extends CharacterBody3D
+
+@export var speed := 6.0
+@export var gravity := 18.0
+
+func _physics_process(delta: float) -> void:
+	var input := Vector2(
+		float(Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_RIGHT)) - float(Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_LEFT)),
+		float(Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_DOWN)) - float(Input.is_key_pressed(KEY_W) or Input.is_key_pressed(KEY_UP))
+	)
+	var direction := Vector3(input.x, 0, input.y).normalized()
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	if not is_on_floor(): velocity.y -= gravity * delta
+	else: velocity.y = -0.1
+	if direction.length() > 0.0: look_at(global_position - direction, Vector3.UP)
+	move_and_slide()
